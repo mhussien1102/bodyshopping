bf02705ebd5e4dabeebb6a495f587bd7471a1b8f
MohammedHussien1103 <bodyalhoseny@gmail.com>
2023-08-28 12:55:05 +0300

    shopping

---
+++ Js/main.js
@@ -0,0 +1,12 @@
+let student = prompt("are you student ?")
+if (student === "yes") {
+                document.write("<h1 class='one'>You can get 50% discount </h1>");
+                document.querySelector(".one").style.textAlign = 'center';
+                document.querySelector(".one").style.fontWeight = "bold";
+                document.querySelector(".one").style.margin = "auto";
+                document.querySelector(".one").style.backgroundColor = "#dc3545";
+                document.querySelector(".one").style.width="fit-content";
+                
+}
+            
+
---
+++ Js/main1.js
@@ -0,0 +1,15 @@
+var user = document.querySelector("user")
+var passw = document.querySelector("passw")
+
+if (user === "M", passw = 123) {
+
+    function windowopen() {
+        window.open("index.html")
+    }
+}
+else {
+    alert ("Wrong Username or Password!")
+}
+
+
+
